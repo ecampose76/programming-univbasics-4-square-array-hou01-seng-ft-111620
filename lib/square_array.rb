7c1d30d28848af10d98290e426a9e569fb862d79
Elias Campos <ecampose76@gmail.com>
2020-11-03 01:17:01 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,6 +1,18 @@
 array = [1, 2 ,3]
 
 def square_array(array)
-  while 
+  new_array = []
+  
+  
 end
 
+array = [1, 2, 3]
+
+def square_array(array)
+  new_array = []
+  
+  array.length.times do |index|
+    new.array.push(array[index] ** 2)
+  end
+  new_array
+end
