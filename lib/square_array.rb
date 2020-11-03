39bdff9eac545e9b9595f109f1a9d295d918f995
Elias Campos <ecampose76@gmail.com>
2020-11-03 01:18:02 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -3,7 +3,7 @@ array = [1, 2 ,3]
 def square_array(array)
   new_array = []
   array.length.times do |index|
-    new.array.push(array[index] ** 2)
+    new_array.push(array[index] ** 2)
   end
   new_array
 end
